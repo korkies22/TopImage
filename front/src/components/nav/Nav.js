ba714f8e37e2cc97e812b45d10743d2e8847bbb3
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-10-30 20:49:32 -0500

    Responsive navbar?

--- front/src/components/nav/Nav.js
+++ front/src/components/nav/Nav.js
@@ -7,13 +7,15 @@ import { logout as deleteUserInfo } from "../../util/state/localStorageUtil";
 
 
 import {
+  Link,
   NavLink
 } from "react-router-dom";
 
 import PropTypes from "prop-types";
 
-function Nav(props) {
+function Nav() {
   const [isUserModalOpen, setUserModalOpen] = useState(false);
+  const [isMobile,setIsMobile] = useState(false);
   const dispatch = useDispatch();
 
   const logout = () => {
@@ -22,12 +24,18 @@ function Nav(props) {
   };
 
   return (
-    <div className="nav">
-      <h2 className="nav__logo">TOP IMAGE</h2>
-      <div className="nav__actions">
+    <nav className={`nav ${isMobile?'nav--active':''}`}>
+      <div className="nav__header">
+        <Link className="nav__logo" to="/">
+          TOP IMAGE
+        </Link>
+        <button class="nav__toggle" onClick={()=>{setIsMobile(!isMobile)}}>
+              <img src={require("../../assets/icons/menu.svg")} alt="Toggler for menu when on mobile"></img>
+          </button>
+      </div>
+      <div className={`${isMobile?'nav__actions--active':'nav__actions'}`}>
         <NavLink exact={true} className="nav__link" activeClassName='nav__link--active' to="/">Home</NavLink>
         <NavLink exact={true} className="nav__link" activeClassName='nav__link--active' to="/search">Search</NavLink>
-        
         <div className="nav__user">
           <a href='/tutorial' target="_blank"> <img className="nav__userIcon" src={require("../../assets/icons/help.svg")} alt="help"/></a>
           <img
@@ -36,6 +44,10 @@ function Nav(props) {
             alt="user"
             onClick={() => setUserModalOpen(!isUserModalOpen)}
           />
+        </div>
+        <div className="nav__user--responsive">
+          <NavLink exact={true} className="nav__link" activeClassName='nav__link--active' to="/tutorial">Tutorial</NavLink>
+          <button onClick={logout} className="nav__mockLink" >Log Out</button>
         </div>
       </div>
 
@@ -46,7 +58,7 @@ function Nav(props) {
           </p>
         </div>
       ) : null}
-    </div>
+    </nav>
   );
 }
 
