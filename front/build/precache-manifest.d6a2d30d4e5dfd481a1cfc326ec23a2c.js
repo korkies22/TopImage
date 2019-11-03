aa319497009f003b819bdc808ca098f525b73a3d
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-11-02 22:44:50 -0500

    Build dir

---
+++ front/build/precache-manifest.d6a2d30d4e5dfd481a1cfc326ec23a2c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba72bc6053b58a99648cb252f3fd834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b68a1d46517ced7a85ae",
+    "url": "/static/css/main.69b5899e.chunk.css"
+  },
+  {
+    "revision": "d3d71e082786928dcfd8",
+    "url": "/static/js/2.0e2e52ec.chunk.js"
+  },
+  {
+    "revision": "b68a1d46517ced7a85ae",
+    "url": "/static/js/main.cb866669.chunk.js"
+  },
+  {
+    "revision": "2490115a9c4ae43e4644",
+    "url": "/static/js/runtime~main.e774a519.js"
+  },
+  {
+    "revision": "ade877c3afaf28919598ef5d018c77ee",
+    "url": "/static/media/cameraLogin.ade877c3.jpg"
+  },
+  {
+    "revision": "666dca4953c3cfa74f4393cb52e1b225",
+    "url": "/static/media/cameraMain.666dca49.jpg"
+  },
+  {
+    "revision": "3f1544fcbea7cec760e8a85dcc47867c",
+    "url": "/static/media/dislike.3f1544fc.svg"
+  },
+  {
+    "revision": "921dfa340b84128931a4e928c78435ac",
+    "url": "/static/media/dislikeU.921dfa34.svg"
+  },
+  {
+    "revision": "840adbfd201d0a090fd21f84d2e6b828",
+    "url": "/static/media/help.840adbfd.svg"
+  },
+  {
+    "revision": "a467c6a087eae74b9a1b7602902b6d31",
+    "url": "/static/media/like.a467c6a0.svg"
+  },
+  {
+    "revision": "08723e079bc49bce212308c3aa7a89af",
+    "url": "/static/media/likeU.08723e07.svg"
+  },
+  {
+    "revision": "5cc607db31f4dc48e7dac3a1f900d31e",
+    "url": "/static/media/logo.5cc607db.svg"
+  },
+  {
+    "revision": "adf7b08c60b741cf837acfb0242f50a5",
+    "url": "/static/media/magnifying-glass.adf7b08c.svg"
+  },
+  {
+    "revision": "aa9d6fc1b2a1017b1a61d7897a01eb11",
+    "url": "/static/media/menu.aa9d6fc1.svg"
+  },
+  {
+    "revision": "0c32117b17dc8c49b53f338a3e6af326",
+    "url": "/static/media/person.0c32117b.svg"
+  }
+]);
