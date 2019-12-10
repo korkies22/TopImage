127428afd456ce88bdba797b17a521542528e4ae
DanielaRocha6 <42350702+DanielaRocha6@users.noreply.github.com>
2019-12-10 11:38:24 -0500

    Code-review
    Además está muy bien modularizado el código. :)

--- front/src/store/accessKey/reducer.js
+++ front/src/store/accessKey/reducer.js
@@ -4,6 +4,7 @@ export default function reducer(state = initState, action) {
   console.log("Action",action.type,action.payload);
   switch (action.type) {
   case "SET_ACCESS_KEY":
+      //Está muy bien protegida la llave. Qué bien!
     return {
       ...state,
       accessKey: action.payload,
